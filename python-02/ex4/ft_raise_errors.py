5f01f452f77bbb2edadb7a41756e4b442110c967
otahiri <chucy0066@gmail.com>
2026-01-02 10:13:25 +0100

    added 04 need more testing

---
+++ python-02/ex4/ft_raise_errors.py
@@ -0,0 +1,30 @@
+def check_plant_health(plant_name, water_level, sunlight_hours):
+    try:
+        if not plant_name:
+            raise ValueError("Error: Plant name cannot be empty!")
+        elif not (1 <= water_level <= 10):
+            raise ValueError(f"Error: Water level {water_level} is too high \
+(max 10)")
+        elif not (2 <= sunlight_hours <= 12):
+            raise ValueError(f"Error: Sunlight hours {sunlight_hours} is too \
+low (min 2)")
+        print(f"Plant '{plant_name}' is healthy!")
+    except ValueError as er:
+        print(er)
+
+
+def test_plant_checks():
+    print("=== Garden Plant Health Checker ===")
+    print("\nTesting good values...")
+    check_plant_health("tomato", 2, 7)
+    print("\nTesting empty plant name...")
+    check_plant_health(None, 2, 5)
+    print("\nTesting bad water level...")
+    check_plant_health("tomato", 15, 9)
+    print("\nTesting bad sunlight hours...")
+    check_plant_health("tomato", 2, 0)
+    print("\nAll error raising tests completed!")
+
+
+if __name__ == "__main__":
+    test_plant_checks()
