499e6b7810434b3de1cdda3c1b3f11c300a00acb
otahiri <chucy0066@gmail.com>
2026-01-24 17:38:29 +0100

    finished the achievment ex3 py03

--- python-03/ft_achievement_tracker.py
+++ python-03/ft_achievement_tracker.py
@@ -1,30 +1,32 @@
-def create_achievments(*args) -> set:
-    return set(args)
 
 
 def main():
     print("=== Achievement Tracker System ===\n")
-    alice = create_achievments('first_kill', 'level_10', 'treasure_hunter',
-                               'speed_demon')
-    bob = create_achievments('first_kill', 'level_10', 'boss_slayer',
-                             'collector')
-    charlie = create_achievments('level_10', 'treasure_hunter', 'boss_slayer',
-                                 'speed_demon', 'perfectionist')
-    print(f"Player alice achievements: {alice}")
+    alice = set(["first_kill", "level_10", "treasure_hunter",
+                "speed_demon"])
+    bob = set(["first_kill", "level_10", "boss_slayer",
+              "collector"])
+    charlie = set(["level_10", "treasure_hunter", "boss_slayer",
+                  "speed_demon", "perfectionist"])
+    print([f"Player alice achievements: {alice}"])
     print(f"Player bob achievements: {bob}")
     print(f"Player charlie achievements: {charlie}")
     print("\n=== Achievement Analytics ===")
     total = alice.union(bob, charlie)
     common = alice.intersection(bob, charlie)
-    rare = bob.difference(charlie)
-    alice_bob = alice & bob
-    unique_alice = alice - (bob | charlie)
+    rare = bob.difference(alice, charlie) | alice.difference(charlie, bob)\
+        | charlie.difference(alice, bob)
+    alice_bob = alice.intersection(bob)
+    unique_alice = alice.difference(bob)
+    unique_bob = bob.difference(alice)
     print(f"All unique achievements: {total}")
     print(f"Total unique achievements: {len(total)}")
     print(f"\nCommon to all players: {common}")
     print(f"Rare achievements (1 player): {rare}")
+    print()
     print(f"Alice vs Bob common: {alice_bob}")
     print(f"Alice unique: {unique_alice}")
+    print(f"Alice unique: {unique_bob}")
 
 
 if __name__ == "__main__":
