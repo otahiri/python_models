f32755fa98d8f6434fe85fdee9d193b96c4c03c3
otahiri <chucy0066@gmail.com>
2026-02-01 16:47:58 +0100

    added some stuff need to fix the ex0 according to the correction sheet

--- python-05/ex1/data_stream.py
+++ python-05/ex1/data_stream.py
@@ -25,11 +25,18 @@ class DataStream(ABC):
 
 
 class SensorStream(DataStream):
-    def __init__(self, stream_id: int) -> None:
+    def __init__(self, stream_id: str) -> None:
         self.stream_id = stream_id
+        self.type = "temp"
+        print(f"Stream ID: {stream_id}, Type: Financial Data")
 
     def process_batch(self, data_batch: List[Any]) -> str:
-        return ""
+        res = {"temp": [0, 0], "humidity": [0, 0], "pressure": [0, 0]}
+        for sensor, value in data_batch:
+            if sensor in res.keys():
+                res[sensor][0] += 1
+                res[sensor][1] += value
+        return f"avg {self.type}: {res[self.type][0] / res[self.type][1]}"
 
     def filter_data(self, data_batch: List[Any],
                     criteria: Optional[str] = None) -> List[Any]:
@@ -43,31 +50,40 @@ class TransactionStream(DataStream):
     def __init__(self, stream_id: str, budget: int) -> None:
         self.stream_id = stream_id
         self.budget = budget
-        self.transactions = dict()
-        print("Initializing Transaction Stream...")
+        self.transactions: Dict[str, Union[str, int, float]] =\
+            {"buy": 0, "sell": 0}
+        print("\nInitializing Transaction Stream...")
         print(f"Stream ID: {stream_id}, Type: Financial Data")
 
     def process_batch(self, data_batch: List[Any]) -> str:
-        for action, _, cost in data_batch:
+        for action, cost, _ in data_batch:
             if action == "buy":
-                self.budget += cost
-            elif action == "sell":
                 self.budget -= cost
+                self.transactions["buy"] += cost
+            elif action == "sell":
+                self.budget += cost
+                self.transactions["sell"] += cost
         return "critical_error: insufficent funds for all transactions" \
             if self.budget < 0 else "sufficent funds for all transactions"
 
     def get_stats(self) -> Dict[str, Union[str, int, float]]:
-        return f"critical: surpassed the budget by {self.budget}"
+        return self.transactions
 
     def filter_data(self, data_batch: List[Any],
                     criteria: Optional[str] = None) -> List[Any]:
         valid_data = list()
+        min_price = 0
+        try:
+            min_price = int(criteria) if criteria else 0
+        except (ValueError):
+            print("Warning: invlaid criteria")
         for data in data_batch:
-            if isinstance(data, tuple) and len(data) == 3:
-                action, item, _ = data
-                if not criteria or item == criteria and \
-                        action in ["buy", "sell"]:
+            try:
+                _, cost, _ = data
+                if not criteria or cost > min_price:
                     valid_data.append(data)
+            except (ValueError, TypeError):
+                pass
         return valid_data
 
 
@@ -136,12 +152,26 @@ def main() -> None:
                                 404,
                                 None
                             ]
-    event_stream = EventStream("EVENT001")
-    transaction_stream = TransactionStream
-    steam_processor = StreamProcessor()
-    steam_processor.add_stream(event_stream)
-    steam_processor.proccess_data("EVENT001", event_input, "error")
+    transaction_input = [("buy", 800, "seed"),
+                         ("sell", 322, "chickens"),
+                         ("buy", 281, "fertilizer"),
+                         ("buy", 91, "chicken_feed"),
+                         ("sell", 183, "eggs")]
+    sensor_input = [("temp", 28.7), ("temp", 12), ("temp", 9), ("temp", 19),
+                    ("humidity", 65), ("humidity", 75), ("humidity", 55),
+                    ("humidity", 70), ("pressure", 1013), ("pressure", 1200),
+                    ("pressure", 1010), ("pressure", 950)]
 
+    event_stream = EventStream("EVENT001")
+    stream_processor = StreamProcessor()
+    stream_processor.add_stream(event_stream)
+    stream_processor.proccess_data("EVENT001", event_input, "error")
+    transaction_stream = TransactionStream("TRANS_001", 9600)
+    stream_processor.add_stream(transaction_stream)
+    stream_processor.proccess_data("TRANS_001", transaction_input, "100")
+    sensor_processor = SensorStream("SENSOR_001")
+    stream_processor.add_stream(sensor_processor)
+    stream_processor.proccess_data("SENSOR_001", sensor_input, "temp")
 
 
 if __name__ == "__main__":
