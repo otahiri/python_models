1d101b791f44841893b9e8f7fa8825c41e36c7c0
otahiri <chucy0066@gmail.com>
2026-02-08 14:53:30 +0100

    added some everything for python6

--- python-06/alchemy/grimoire/spellbook.py
+++ python-06/alchemy/grimoire/spellbook.py
@@ -1,6 +1,12 @@
+# importing the entire module to avoid circular import
+import alchemy.grimoire
+
+
 def record_spell(spell_name: str, ingredients: str) -> str:
-    from ..grimoire import validate_ingredients
-    result = validate_ingredients(ingredients)
+    # late importing to avoid circular import
+    from ..grimoire import validator
+    result = validator.validate_ingredients(ingredients)
+    result = alchemy.grimoire.validate_ingredients(ingredients)
     if "INVALID" in result:
         return f"Spell rejected: {spell_name} ({result})"
     else:
