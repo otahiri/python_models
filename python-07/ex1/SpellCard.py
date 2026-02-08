9816e1a1a42fb9d424412432791f0a23379d4e57
otahiri <chucy0066@gmail.com>
2026-02-08 22:25:14 +0100

    need more work on 07

--- python-07/ex1/SpellCard.py
+++ python-07/ex1/SpellCard.py
@@ -1,1 +1,14 @@
-pass
+from ex0 import Card
+
+
+class SpellCard(Card):
+    def __init__(self, name: str, cost: int, rarity: str,
+                 effect_type: str) -> None:
+        super().__init__(name, cost, rarity)
+        self.effect_type = effect_type
+
+    def play(self, game_state: dict) -> dict:
+        return super().play(game_state)
+
+    def resolve_effect(self, targets: list) -> dict:
+        pass
