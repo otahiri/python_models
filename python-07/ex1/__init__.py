f202f4d00ff377bb0552631bbf144771ab6dce61
otahiri <chucy0066@gmail.com>
2026-02-09 15:13:38 +0100

    added some more stuff the main does not work need to fix the import

--- python-07/ex1/__init__.py
+++ python-07/ex1/__init__.py
@@ -1,1 +1,5 @@
-pass
+from Deck import Deck
+from ArtifactCard import ArtifactCard
+from SpellCard import SpellCard
+from ex0.CreatureCard import CreatureCard
+__all__ = ["Deck", "ArtifactCard", "SpellCard", "CreatureCard"]
--- python-07/ex1/main.py
+++ python-07/ex1/main.py
@@ -1,1 +1,18 @@
-pass
+from ex1 import Deck, SpellCard, ArtifactCard, CreatureCard
+
+
+def main() -> None:
+    deck = Deck()
+    print("\n=== DataDeck Deck Builder ===\n")
+    print("Building deck with different card types...")
+    dragon = CreatureCard("Fire Dragon", 5, "Legendary", 5, 10)
+    lightning = SpellCard("Lightning Bolt", 3, "Legendary", "Damage")
+    mana_crystal = ArtifactCard("Mana Crystal", 2, "Common", 3, "Buff", )
+    deck.add_card(dragon)
+    deck.add_card(lightning)
+    deck.add_card(mana_crystal)
+    print(f"Deck stats: {deck.get_deck_stats()}")
+
+
+if __name__ == "__main__":
+    main()
