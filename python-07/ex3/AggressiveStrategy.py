b79ce0ada12f4874f8d13ab8699df7c29d92229a
otahiri <chucy0066@gmail.com>
2026-02-14 18:35:08 +0100

    added the final touches to ex3 py7

--- python-07/ex3/AggressiveStrategy.py
+++ python-07/ex3/AggressiveStrategy.py
@@ -2,12 +2,19 @@ from typing import Dict
 from ex0 import Card
 from ex3 import GameStrategy
 from ex1 import CreatureCard
+from enum import Enum
+
+
+class Attack(Enum):
+    COMMON = 1
+    RARE = 2
+    LEGENDARY = 3
+    MYTHIC = 5
 
 
 class AggressiveStrategy(GameStrategy):
 
     def execute_turn(self, hand: list, battlefield: list) -> dict:
-        atk = {"common": 1, "rare": 2, "legendary": 3, "mythic": 5}
         targets = self.prioritize_targets(battlefield)
         res: Dict = dict()
         res["cards_played"] = [card.name for card in hand
@@ -15,7 +22,8 @@ class AggressiveStrategy(GameStrategy):
         res["mana_used"] = sum([card.cost for card in hand if
                                 isinstance(card, Card)])
         res["targets_attacked"] = [card.name for card in targets]
-        res["damage_dealt"] = sum(atk[card.rarity] for card in hand
+        res["damage_dealt"] = sum(Attack.__getitem__(card.rarity.upper()).value
+                                  for card in hand
                                   if isinstance(card, Card)) * len(targets)
         return res
 
