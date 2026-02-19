41d7ca62e4eb31ebe5f11d84767d0541263edf25
otahiri <chucy0066@gmail.com>
2026-02-19 16:41:26 +0000

    added some inintal stuff for the ex2 py10

--- python-10/ex2/scope_mysteries.py
+++ python-10/ex2/scope_mysteries.py
@@ -1,1 +1,32 @@
-pass
+def mage_counter() -> callable:
+    count = 0
+
+    def counter() -> int:
+        nonlocal count
+        count += 1
+        return count
+    return counter
+
+
+def spell_accumulator(initial_power: int) -> callable:
+    pass
+
+
+def enchantment_factory(enchantment_type: str) -> callable:
+    pass
+
+
+def memory_vault() -> dict[str, callable]:
+    pass
+
+
+def main():
+    count = mage_counter()
+    print(count)
+    count = mage_counter()
+    print(count)
+    count = mage_counter()
+    print(count)
+
+
+main()
